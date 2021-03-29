c6d7fe2ab9ee6ca4b77d443b35b32fdee0b1ea2d
Cody Richter <Cody@Richter.codes>
2021-03-29 11:45:42 -0400

    Remove deprecated node sass dependency

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import { ThemeProvider } from '@material-ui/styles';
 
 import theme from './theme';
 import 'react-perfect-scrollbar/dist/css/styles.css';
-import './assets/scss/index.scss';
 import Routes from './Routes';
 
 const browserHistory = createBrowserHistory();
