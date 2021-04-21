ef4ff6b919b6012e5048130a5d86e0274a8e0fd0
Chris <chrdoan@umass.edu>
2021-04-21 12:04:53 -0400

    working video model

--- prediction/models/SpeechRec_NERMicroservice/config.py
+++ prediction/models/SpeechRec_NERMicroservice/config.py
@@ -5,7 +5,7 @@ model_name = 'speech_rec_ner'
 # Tags are used to describe the performance of a model. These simple keywords can help people decide whether your model
 # is appropriate to use for their situation. Some examples of tags are 'fast', 'accurate', or 'essential'. You should
 # limit the number of tags your model has to only contain a few with relevant information.
-model_tags = ['video', 'slow', 'NER']
+model_tags = 'video,slow,NER'
 
 
 # The model type determines what inputs your model will receive. The options are:
