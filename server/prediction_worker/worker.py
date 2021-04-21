a322b5e93a83eac42b830a3e3dc5789a33e6a0fa
Cody Richter <Cody@Richter.codes>
2021-04-21 10:09:02 -0400

    Ensure global init is working correctly

--- server/prediction_worker/worker.py
+++ server/prediction_worker/worker.py
@@ -1,8 +1,8 @@
 import uuid
 from redis import Redis
 from model.config import model_name
-from rq import Queue, Worker, Connection
-from model.model import init
+from rq import Queue, SimpleWorker, Connection
+from model.model import init, predict
 from utility import main
 from concurrent.futures.thread import ThreadPoolExecutor
 
@@ -14,12 +14,11 @@ pool = ThreadPoolExecutor(10)
 if __name__ == '__main__':
     print('Starting Worker')
     unique_worker_id = str(uuid.uuid4())
-    init()  # Ensure that the model is ready to receive predictions.
-    pool.submit(main.register_to_server)  # Add this model to the server's list of available models.
-    print('[Worker] Registration and Model Initiation Complete')
     with Connection(redis):
         queue = Queue(model_name)
-        worker = Worker([queue], connection=redis, name='Worker'+model_name+'---'+unique_worker_id)
+        init()  # Ensure that the model is ready to receive predictions.
+        pool.submit(main.register_to_server)  # Add this model to the server's list of available models.
+        worker = SimpleWorker([queue], connection=redis, name='Worker'+model_name+'---'+unique_worker_id)
         worker.work()
     shutdown = True
     print('Ending Worker')
